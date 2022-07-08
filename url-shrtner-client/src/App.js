b386b9b161336ea5ee50dcd8978dbda26c5d9f16
Jonathan Lee <jonathanlee0129@berkeley.edu>
2022-07-07 22:59:10 -0700

    done for day

--- url-shrtner-client/src/App.js
+++ url-shrtner-client/src/App.js
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
 import Form from './components/Form';
-
 function App() {
   return (
-    <Form></Form>
+    <Form/>
   );
 }
 
--- url-shrtner-client/src/components/Form.js
+++ url-shrtner-client/src/components/Form.js
@@ -1,42 +1,71 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { Container } from '@mui/system';
+import Button from '@mui/material/Button';
 import { Typography } from '@mui/material';
+import TextField from "@mui/material/TextField";
+import InputAdornment from '@mui/material/InputAdornment';
 const Form = () => {
-    return(
-
-
-    <Box>
-        <Container
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Typography
-        variant="h2"
-        sx={{
-          fontWeight: "300",
-          margin: "0px auto 0px auto",
-          lineHeight: "100%",
-        }}
-      >
-        SHORT
+    return (
         <Box
-          component="span"
-          sx={{
-            fontWeight: "900",
-            alignItems: "center",
-          }}
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                margin: "30vh 10px 0px 10px",
+            }}
         >
-          LINK
+            <Box
+                sx={{
+
+                    display: 'flex',
+                    flexDirection: 'column',
+                    borderRadius: '5px',
+
+
+                }}
+            >
+                <Typography
+                    variant="h2"
+                    sx={{
+                        fontWeight: "300",
+                        margin: "0px auto 0px auto",
+                        lineHeight: "100%",
+                        marignBottom: "10px",
+                    }}
+                >
+                    SHORT
+                    <Box
+                        component="span"
+                        sx={{
+                            fontWeight: "900",
+                            alignItems: "center",
+                        }}
+                    >
+                        LINK
+                    </Box>
+                </Typography>
+
+
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Enter Your Long URL"
+                />
+
+                <TextField
+                    id="outlined-start-adornment"
+                    placeholder="eg. ECQ!YC (optional)"
+                    InputProps={{
+                        startAdornment: <InputAdornment position="start">https://ShortLink.com/</InputAdornment>,
+                    }}
+                />
+                <Button
+                    margin='normal'
+                    variant="contained">Generate ShortLink!</Button>
+
+            </Box >
+
         </Box>
-      </Typography>
-    </Container>
-    </Box>
-        
 
     );
 
